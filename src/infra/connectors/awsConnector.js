f4fd736ce0f056cdbf6a97ed872ccd9c6f67c3e2
Trung Luu <luuductrung1234@gmail.com>
2024-12-14 11:53:43 +0700

    enhance load secrets

--- src/infra/connectors/awsConnector.js
+++ src/infra/connectors/awsConnector.js
@@ -1,5 +1,9 @@
 const { STSClient, GetCallerIdentityCommand } = require("@aws-sdk/client-sts");
-const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
+const {
+  SSMClient,
+  GetParameterCommand,
+  GetParametersCommand,
+} = require("@aws-sdk/client-ssm");
 
 var _localOnlySecrets = {
   "/shop/userPoolId": process.env.LOCAL_ONLY_COGNITO_USER_POOL_ID,
@@ -19,9 +23,24 @@ var _remoteSecrets = {};
 module.exports.loadSecrets = async () => {
   await callerCheck();
 
+  const client = new SSMClient({
+    region: process.env.REGION_STR,
+  });
+  const response = await client.send(
+    new GetParametersCommand({
+      Names: Object.keys(_localOnlySecrets),
+      WithDecryption: true,
+    })
+  );
+
   for (var key in _localOnlySecrets) {
     console.log("Load secret value with key: ", key);
-    _remoteSecrets[key] = await resolveSecretValue(key);
+    let parameter = response.Parameters.find((p) => p.Name === key);
+    console.log(
+      "Retrieved secret value from SSM Pamaeter Store: ",
+      parameter.ARN
+    );
+    _remoteSecrets[key] = parameter.Value;
   }
 
   console.log("Successfully load all secrets.");
@@ -43,6 +62,9 @@ const callerCheck = async () => {
   );
 };
 
+/**
+ * @deprecated
+ */
 const resolveSecretValue = async (secretName) => {
   if (process.env.NODE_ENV === "local") return _localOnlySecrets[secretName];
 
--- src/migrate.js
+++ src/migrate.js
@@ -1,6 +1,9 @@
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+const { loadSecrets } = require("./infra/connectors/awsConnector");
 
-/**
- * @see https://github.com/sequelize/umzug/tree/main#cli
- */
-require("./infra/umzug").migrator.runAsCLI();
+loadSecrets().then(() => {
+  /**
+   * @see https://github.com/sequelize/umzug/tree/main#cli
+   */
+  require("./infra/umzug").migrator.runAsCLI();
+});
